b4469f0d9c11bf9ab8d25bbfe135b9328bfff468
FilaCo <dvchizha@edu.hse.ru>
2019-04-07 16:45:43 +0500

    Начало работы с XPath

---
+++ XPathWork/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Compression;
+
+namespace XPathWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZipFile.ExtractToDirectory("Current.zip", "CurrentUnpackaged");
+        }
+    }
+}
